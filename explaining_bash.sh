b2d5f34c697d5091fd9726d0a9de1f0b29359921
Jane333 <jana.cavojska9@gmail.com>
2015-05-11 03:07:16 +0200

    added explaining_bash.sh

---
+++ explaining_bash.sh
@@ -0,0 +1,39 @@
+*******  ABOUT BASH  *******
+
+What is bash?
+ - scripting language
+ - interpreter for the bash scripting language
+ - what sits behind a terminal emulator
+ - binary executable to be found at /bin/bash
+
+What is it good for?
+ - calling programs like cool linux tools
+ - it is interactive, so interacting with called programs
+ - connecting multiple programs via pipes
+ - writing simple scripts or more complex programs
+
+Starting linux tools from bash:
+	ls
+	cd
+	pwd
+	cp
+	mv
+	find
+	grep
+
+Variables
+if-then-else
+reading files line by line
+doing stuff for all files in directory
+((math))
+
+pipes, I/O redirecting - examples
+
+
+********  REGULAR EXPRESSIONS  *********
+
+.*+?{}
+[](|)
+^$
+\1
+lookarounds
